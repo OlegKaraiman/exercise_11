289b6782a5b1964663dbab158c72b257a47c13db
OlegKaraiman <59221843+OlegKaraiman@users.noreply.github.com>
2020-04-04 20:35:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,66 @@
-
-
 // eslint-disable-next-line no-unused-vars
 function Student(firstName, secondName, bdYear) {
-    const arrHeтgth = 30;//размер массива
-    this.firstName = firstName; //имя
-    this.secondName = secondName; //фамилия
-    this.bdYear = bdYear; //год рождения
-    this.presentAbsent = new Array(arrHeтgth); //массив посещения 30 символов
-    this.rating = new Array(arrHeтgth); //массив с оценками 30 символов
-    this.age = () => new Date().getFullYear() - bdYear; //метод возраст
-    this.present = function() {
-        for (let i = 0; i < this.presentAbsent.length; i++) {
-            if (typeof this.presentAbsent[i] === 'undefined') {
-                this.presentAbsent[i] = true;
-                break;
-            }
-        }
-    }; //метод посещения
-    this.absent = function() {
-        for (let i = 0; i < this.presentAbsent.length; i++) {
-            if (typeof this.presentAbsent[i] === 'undefined') {
-                this.presentAbsent[i] = false;
-                break;
-            }
-        }
-    }; //метод отсутствия
-    this.mark = function(sum) {
-        for (let i = 0; i < this.rating.length; i++) {
-            if (typeof this.rating[i] === 'undefined') {
-                if (sum <= '10' && sum > '0') {
-                    this.rating[i] = sum;
-                }
-                break;
-            }
-        }
-    }; //метод ввод оценок
+  const arrLength = 30; //размер массива
+  this.firstName = firstName; //имя
+  this.secondName = secondName; //фамилия
+  this.bdYear = bdYear; //год рождения
+  this.presentAbsent = new Array(arrLength); //массив посещения 30 символов
+  this.rating = new Array(arrLength); //массив с оценками 30 символов
+  this.age = () => new Date().getFullYear() - bdYear; //метод возраст
 
-    this.summaryRating = function() {
-        let sum = 0;
-        let sumVol = 0;
-        for (let i = 0; i < this.rating.length; i++) {
-            if (isNaN(this.rating[i])) {
-                sum++;
+  const isPresent = element => typeof element === "undefined";
 
-            } else {
-                sumVol += this.rating[i];
-            }
-        }
-        return sumVol / (this.rating.length - sum);
-    }; //метод подсчета средней оценки
+  this.present = function() {
+    let elemIndex = this.presentAbsent.findIndex(isPresent);
+    this.presentAbsent[elemIndex] = true;
+  }; //метод посещения
 
-    this.summaryPresent = function() {
-        let sum = 0;
-        let sumVol = 0;
-        for (let i = 0; i < this.presentAbsent.length; i++) {
-            if (isNaN(this.presentAbsent[i])) {
-                sum++;
-            } else {
-                sumVol += this.presentAbsent[i];
-            }
-        }
-        return sumVol / (this.presentAbsent.length - sum);
-    }; //подсчет среднего посещение
+  const isAbsent = element => typeof element === "undefined";
 
-    this.summary = function() {
-        const present = 0.9;
-        const rating = 9;
-        if (this.summaryPresent() >= present && this.summaryRating() >= rating) {
-            return 'Ути какой молодец';
-        } else if (
-            (this.summaryPresent() < present && this.summaryRating() >= rating) ||
+  this.absent = function() {
+    let elemIndex = this.presentAbsent.findIndex(isAbsent);
+    this.presentAbsent[elemIndex] = false;
+  }; //метод отсутствия
+
+  const isMark = element => typeof element === "undefined";
+
+  this.mark = function(sum) {
+    let elemIndex = this.rating.findIndex(isMark);
+    this.rating[elemIndex] = sum;
+  }; //метод ввод оценок
+
+  this.summaryRating = function() {
+    let sumVol = 0;
+    let sumRuns = 0;
+    this.rating.forEach(function(element) {
+      sumVol += element;
+      sumRuns++;
+    });
+    return sumVol / sumRuns;
+  }; //метод подсчета средней оценки
+
+  this.summaryPresent = function() {
+    let sumVol = 0;
+    let sumRuns = 0;
+    this.presentAbsent.forEach(function(element) {
+      sumVol += element;
+      sumRuns++;
+    });
+    return sumVol / sumRuns;
+  }; //подсчет среднего посещение
+
+  this.summary = function() {
+    const present = 0.9;
+    const rating = 9;
+    if (this.summaryPresent() >= present && this.summaryRating() >= rating) {
+      return "Ути какой молодец";
+    } else if (
+      (this.summaryPresent() < present && this.summaryRating() >= rating) ||
       (this.summaryPresent() >= present && this.summaryRating() < rating)
-        ) {
-            return 'Норм, но можно лучше';
-        } else {
-            return 'Редиска';
-        }
-    };
+    ) {
+      return "Норм, но можно лучше";
+    } else {
+      return "Редиска";
+    }
+  };
 } //метод проверка средней оценки и среднего посещения
-
-
-
